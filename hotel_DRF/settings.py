67b51b411262fffb3d29602fe90c394995858427
Alex Golub <102504564+bookslomot@users.noreply.github.com>
2022-07-28 19:32:38 +0400

    Update settings.py

--- hotel_DRF/settings.py
+++ hotel_DRF/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9qmy(nsu9@u)484sld9nk4l$dkn(k18^)%8i7doymm7hd85@5o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
